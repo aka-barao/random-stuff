bb21bf6f3fd6009570f023de14146db5e5094924
Vinicius Elias <viniciuselias28@hotmail.com>
2024-02-15 22:31:39 -0300

    Inclusão de melhorias
    - Filtragem de URLs durante leitura
    - Inclusão de User-Agent do Google Chrome
    - Status de progresso impressosdurante execução do script
    - Tratamento ao abrir e gravar arquivos para evitar exceções em caso de caracteres inesperados
    - Limpeza do código

--- urls_2_links.py
+++ urls_2_links.py
@@ -2,31 +2,47 @@ import requests
 import sys
 from bs4 import BeautifulSoup
 
-# Cria um arquivo HTML com links para as URLs e seus respectivos titúlos
+# Script que lê URLs de um arquivo de texto e gera um arquivo HTML com seus respectivos links
+# Os links são nomeados com o título da página, facilitando a leitura e visualização para listas grandes de URLs
+
+# User-Agent da uma versão recente do Google Chrome. Alguns sites bloqueiam requisições feitas por scripts, como da biblioteca "requests"
+headers_values = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'}
 
 def get_titles(urls_file):
     titles = []
-    with open(urls_file, 'r') as file:
+    print(f"Lendo URLs do arquivo: {urls_file}\n")
+    with open(urls_file, 'r', encoding='UTF-8', errors='ignore') as file:
         urls = file.readlines()
-        for url in urls:
+        urls = [line for line in urls if line.strip()] # Ignora linhas em branco
+        urls = [line for line in urls if line.startswith('http')] # Ignora linhas que não iniciam em http:// ou https://
+        print(f"URLs lidas: {len(urls)}\n")
+        for index, url in enumerate(urls):
             url = url.strip()
+            print(f"{index + 1}/{len(urls)} - Acessando: {url}")          
             try:
-                response = requests.get(url, verify=True)  # Adiciona verificação de SSL
+                response = requests.get(url, verify=True, headers=headers_values)  # Adiciona verificação de SSL e User-Agent
+                print(f"Status HTTP: {response.status_code}")  
                 soup = BeautifulSoup(response.text, 'html.parser')
                 title = soup.title.string
+                print(f"Titúlo: {title}\n")
                 titles.append((url, title))
             except Exception as e:
-                print(f"Erro ao acessar {url}: {e}")
+                print(f"Erro ao acessar {url}: {e}\n")
     return titles
 
 def generate_html_file(titles, output_file):
-    with open(output_file, 'w') as file:
+    print(f"Gerando arquivo HTML em: {output_file}")
+    with open(output_file, 'w', encoding='UTF-8', errors='ignore') as file:
         file.write("<html>\n")
         file.write("<body>\n")
+        file.write("<h1>Links</h1>\n")
+        file.write("<ol>\n")
         for url, title in titles:
-            file.write(f"<a href='{url}' target='_blank'>{title}</a><br>\n")
+            file.write(f"<li><a href='{url}' target='_blank'>{title}</a></li>\n")
+        file.write("</ol>\n")
         file.write("</body>\n")
         file.write("</html>\n")
+    print("Arquivo HTML gerado com sucesso!\n")
 
 def main():
     if len(sys.argv) != 3:
